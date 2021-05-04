22bf0e80db6f83da4f3ce5d4181f473c541d7e89
hamcheeseburger <yoo77hyeon@gmail.com>
2021-05-04 17:16:08 +0900

    FIRST

---
+++ src/main/java/com/example/supringboot/SupringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.supringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/supringboot/SupringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

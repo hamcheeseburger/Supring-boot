53aed70601a5a396916ce98f7621573f1db75e77
yoouyoou <yny026@gmail.com>
2021-05-17 10:53:29 +0900

    공동구매 등록 (미완성)

---
+++ src/main/java/com/example/supringboot/service/ItemService.java
@@ -0,0 +1,19 @@
+package com.example.supringboot.service;
+
+import java.util.List;
+
+import com.example.supringboot.domain.Food;
+import com.example.supringboot.domain.Item;
+
+public interface ItemService {
+	//공구식품 등록
+	int insertItem(Item item);
+	//공구식품 등록을 위한 Food테이블 서치
+	List<Food> searchFoodList(String keyword);
+	//공구식품 수정
+	int updateItem(Item item);
+	//공구식품 삭제
+	int deleteItem(int item_id);
+	//공구진행률 조회
+	int progressItem(int item_id);
+}

bed3b6cdc6e791bebbb713ba829c3de7b4f4b487
hamcheeseburger <yoo77hyeon@gmail.com>
2021-05-21 21:14:25 +0900

    [ADD] signOnInterceptor 추가

--- src/main/java/com/example/supringboot/MvcConfig.java
+++ src/main/java/com/example/supringboot/MvcConfig.java
@@ -1,38 +1,34 @@
 package com.example.supringboot;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
+import com.example.supringboot.controller.SignOnInterceptor;
 
 //import sp5.sp5chapcboot.interceptor.AuthCheckInterceptor;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-//	@Autowired
-//	private AuthCheckInterceptor authCheckInterceptor;
-
+	
+	@Autowired
+	private SignOnInterceptor interceptor;
+	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("main");
 	}
 
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(authCheckInterceptor)
-//				.addPathPatterns("/edit/**")
-//				.excludePathPatterns("/edit/help/**");
-//	}
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor)
+				.addPathPatterns("/account/**")
+				.excludePathPatterns("/account/signOnForm", "/account/signOff");
+	}
 	
-
-
-
 }

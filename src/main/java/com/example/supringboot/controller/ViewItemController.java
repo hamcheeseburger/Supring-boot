8a353c4a6ffadc38ff79f0856a18dee23af3d62b
hamcheeseburger <yoo77hyeon@gmail.com>
2021-05-30 15:11:52 +0900

    [ADD] itemList category 구현

--- src/main/java/com/example/supringboot/controller/ViewItemController.java
+++ src/main/java/com/example/supringboot/controller/ViewItemController.java
@@ -1,15 +1,18 @@
 package com.example.supringboot.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.supringboot.domain.Category;
 import com.example.supringboot.domain.Item;
 import com.example.supringboot.service.ItemService;
 import com.example.supringboot.service.WishServiceImpl;
@@ -22,27 +25,38 @@ public class ViewItemController {
 	@Autowired
 	private ItemService itemService;
 	
+	@ModelAttribute("categoryList")
+	public ArrayList<Category> categoryList() {
+		return itemService.selectAllCategory();
+	}
+	
 	// 공구 식품 목록 보기
 	@RequestMapping("/item/list")
-	public ModelAndView itemList() {
+	public ModelAndView itemList(@RequestParam(required=false, defaultValue="-1") Integer cat_id) {
+		System.out.println("cat_id : " + cat_id);
+		
 		ModelAndView mav = new ModelAndView("/Item/itemList");
-
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		List<Item> allList = wishService.getAllItem();
-		List<Item> goingList = wishService.getGoingItem();
-		List<Item> endList = wishService.getEndItem();
-
-		map.put("allItemList", allList);
-		map.put("allItemCount", allList.size());
 		
-		map.put("goingItemList", goingList);
-		map.put("goingItemCount", goingList.size());
+		if(cat_id == -1) {
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			List<Item> allList = wishService.getAllItem();
+			List<Item> goingList = wishService.getGoingItem();
+			List<Item> endList = wishService.getEndItem();
 		
-		map.put("endItemList", endList);
-		map.put("endItemCount", endList.size());
+			map.put("allItemList", allList);
+			map.put("allItemCount", allList.size());
+		
+			map.put("goingItemList", goingList);
+			map.put("goingItemCount", goingList.size());
+		
+			map.put("endItemList", endList);
+			map.put("endItemCount", endList.size());
 
-		mav.addObject("map", map);
-
+			mav.addObject("map", map);
+		}else {
+			ArrayList<Item> catItemList = itemService.selectItemWithCategory(cat_id);
+			mav.addObject("catItemList", catItemList);
+		}
 		return mav;
 	}
 	

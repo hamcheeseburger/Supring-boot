22c9cc94f0fc3f12b2d7a456bcd3a66f94c4b4f7
권민채 <minchae075@naver.com>
2021-05-31 05:09:56 +0900

    공구 신청 폼 validator 수정

--- src/main/java/com/example/supringboot/service/ApplyValidator.java
+++ src/main/java/com/example/supringboot/service/ApplyValidator.java
@@ -25,7 +25,24 @@ public class ApplyValidator implements Validator {
 	public void validateCreditCard(ApplyForm order, Errors errors) {
 		errors.setNestedPath("order");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "card_num", "CCN_REQUIRED", "카드 번호를 입력하세요.");
+		
+		// 핸드폰 형식이 이상할 시에 변경
+		String newCardNumFormat = card_num_format(order.getOrder().getCard_num());
+		if(newCardNumFormat.length() > 0 && newCardNumFormat.length() != 19) {
+			errors.rejectValue("card_num", "invalid_card_num", "카드 번호 형식이 올바르지 않습니다. ex) 0000-0000-0000-0000");
+		} else {
+			order.getOrder().setCard_num(newCardNumFormat);
+		}
+		
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "card_exp_dt", "EXPIRY_DATE_REQUIRED", "카드 유효기간을 입력하세요.");
+		
+		String newCardExpFormat = card_exp_format(order.getOrder().getCard_exp_dt());
+		if(newCardExpFormat.length() > 0 && newCardExpFormat.length() != 5) {
+			errors.rejectValue("card_exp_dt", "invalid_card_exp", "유효 기간 형식이 올바르지 않습니다. ex) MM/yy");
+		} else {
+			order.getOrder().setCard_exp_dt(newCardExpFormat);
+		}
+		
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "card_type", "CARD_TYPE_REQUIRED", "카드사를 선택하세요.");
 		errors.setNestedPath("");
 	}
@@ -38,4 +55,15 @@ public class ApplyValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "ship_zip", "ZIP_REQUIRED", "우편번호를 입력하세요.");
 		errors.setNestedPath("");
 	}
+
+	public String card_num_format(String number) { 
+		String regEx = "(\\d{4})(\\d{4})(\\d{4})"; 
+		return number.replaceAll(regEx, "$1-$2-$3-$4");
+	}
+	
+	public String card_exp_format(String number) { 
+		String regEx = "(\\d{2})(\\d{2})"; 
+		return number.replaceAll(regEx, "$1/$2");
+	}
+	
 }

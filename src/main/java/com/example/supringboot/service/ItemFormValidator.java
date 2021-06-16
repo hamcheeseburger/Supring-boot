a65855ebd8ba03ea32fe55ce74bc56fa1ff157eb
yoouyoou <yny026@gmail.com>
2021-06-17 00:14:25 +0900

    공동구매 입력폼 Validator구현

--- src/main/java/com/example/supringboot/service/ItemFormValidator.java
+++ src/main/java/com/example/supringboot/service/ItemFormValidator.java
@@ -2,6 +2,7 @@ package com.example.supringboot.service;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.example.supringboot.controller.ItemForm;
@@ -17,12 +18,18 @@ public class ItemFormValidator implements Validator {
 	@Override
 	public void validate(Object target, Errors errors) {
 		ItemForm itemForm = (ItemForm)target;
+		
 		try {
-//			공백이거나 소수가 들어왔을때 에러 삽입
-			int item_price = Integer.parseInt(itemForm.getStr_item_price());
-			if (item_price <= 0) {
-				errors.rejectValue("str_item_price", "numberFormatException", "1 이상의 정수만 입력 가능합니다.");
-			}
+			//키워드 검색이 먼저 수행되어야 함을 확인하는
+			
+//			//상품금액, 배송비 빈칸 검사
+//			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "item_price", "thissss", "상품금액은 필수 입력 값입니다.");
+//			
+//			
+//			int item_price = Integer.parseInt(itemForm.getStr_item_price());
+//			if (item_price <= 0) {
+//				errors.rejectValue("str_item_price", "numberFormatException", "1 이상의 정수만 입력 가능합니다.");
+//			}
 		} catch (Exception e) {
 			errors.rejectValue("str_item_price", "numberFormatException", "1 이상의 정수만 입력 가능합니다.");
 		}
